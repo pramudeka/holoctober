f88fe60a8d3cdfc6c8707af4701ac45929f4170e
pramudeka <goughparliecke@gmail.com>
2023-09-29 11:33:06 +0700

    begin

---
+++ src/components/randomizer/App.vue
@@ -0,0 +1,32 @@
+<script>
+// import { RouterView } from 'vue-router'
+import HomeView from './view/Home.vue'
+import NotFoundView from './view/NotFound.vue'
+
+const routes = {
+  '/': HomeView,
+  '/redirect/link':HomeView
+}
+
+export default {
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || '/'] || NotFoundView
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+		  this.currentPath = window.location.hash
+		})
+  }
+}
+</script>
+<template>
+    <!-- <RouterView/> -->
+    <component :is="currentView" />
+</template>
---
+++ src/components/randomizer/view/Home.vue
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<template>
+    <main>
+        <h1>Hello Wolrds</h1>
+    </main>
+</template>
+
+<style>
+main{
+    width: 100%;
+}
+</style>
